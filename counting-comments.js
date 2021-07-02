faaf2b99076f8783508b330026fe85f587b268a4
quantum-chooz <86805730+quantum-chooz@users.noreply.github.com>
2021-07-01 23:00:01 -0500

    Update counting-comments.js

--- counting-comments.js
+++ counting-comments.js
@@ -4,30 +4,25 @@ var occurences = 0;
 var count = 0;
 var offset = -40;
 var link = 'https://api.scratch.mit.edu/studios/5842709/comments/?offset=' + String(offset) + '&limit=40';
+var t = 0;
 function getPageJSON(a, link) {
-	$.getJSON(link, function(data) {
-		offset +=40;
+	setTimeout(function() {
+		$.getJSON(link, function(data) {
+			offset +=40;
 
-		// var link = 'https://api.scratch.mit.edu/studios/5842709/comments/?offset=' + String(offset) + '&limit=40';
-		for (let i = 0; i < 40; i ++) {
-			console.log(occurences);
-			if (data[i].author.username == user) {
-				++ occurences
+			for (let i = 0; i < 40; i ++) {
+				console.log(occurences);
+				if (data[i].author.username == user) {
+					++ occurences
+				}
 			}
-		}
-		console.log('comments since ' + String(data[39].datetime_created));
-	});
+			console.log('comments since ' + String(data[39].datetime_created));
+		});
+	}, t * 5000);
 }
 
-(function (next, t) {
-	setTimeout(function() {
-		for (let a = 0; a < pages; a ++) {
-			offset +=40;
-			var link = 'https://api.scratch.mit.edu/studios/5842709/comments/?offset=' + String(offset) + '&limit=40';
-			getPageJSON(a, link);
-		}
-
-	}, t * 5000);
-	next() // this part isn't working but whatever
-}(function() {
-}))
+for (let a = 0; a < pages; a ++) {
+	offset +=40;
+	var link = 'https://api.scratch.mit.edu/studios/5842709/comments/?offset=' + String(offset) + '&limit=40';
+	getPageJSON(a, link);
+}
